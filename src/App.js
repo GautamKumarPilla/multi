8f7e74d63779aec6cf8626a14e6a4b4d0cfe1714
GAUTAM KUMAR PILLA <you@example.com>
2023-02-04 17:08:28 +0530

    MultipleComponents

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './Counter';
+import Todolist from './Todolist';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='border border-3 m-2 p-2 border-danger'>
+      <h1>Welcome to Edupoly ReactJS</h1>
+      <Counter/>
+      <Todolist/>
     </div>
-  );
-}
-
-export default App;
+  )
+  }
+  export default App;
