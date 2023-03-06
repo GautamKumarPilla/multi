fbe0510849ddd6412b0a589a4df2a26726eadea7
GAUTAM KUMAR PILLA <you@example.com>
2023-03-06 13:18:37 +0530

    POS

--- src/index.js
+++ src/index.js
@@ -3,12 +3,43 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import '../node_modules/bootstrap/dist/js/bootstrap.min'
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Components/Home';
+import Billing from './features/Billing/Billing';
+import Sales from './features/Sales/Sales';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children:[
+      {
+        path:'/billing',
+        element:<Billing></Billing>
+      },
+      {
+        path:'/home',
+        element:<Home></Home>
+      },
+      {
+        path:'/sales',
+        element:<Sales></Sales>
+      },
+      {
+        path:'/',
+        element:<Home></Home>
+      }
+    ]
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App/> 
-); 
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
