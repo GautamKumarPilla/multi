9dd272b7b0c96c83e6872bb45d6c04751adb312d
GAUTAM KUMAR PILLA <you@example.com>
2023-03-14 22:41:18 +0530

    .

--- src/index.js
+++ src/index.js
@@ -3,37 +3,44 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import '../node_modules/bootstrap/dist/js/bootstrap.min';
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import Home from './Components/Home';
 import Billing from './features/Billing/Billing';
-
+import Sales from './features/Sales/Sales';
 const router = createBrowserRouter([
   {
     path: "/",
-    element:<App></App>,
+    element: <App></App>,
     children:[
       {
-        path:"/home",
+        path:'/billing',
+        element:<Billing></Billing>
+      },
+      {
+        path:'/home',
         element:<Home></Home>
       },
       {
-        path:"/billing",
-        element:<Billing></Billing>
+        path:'/sales',
+        element:<Sales></Sales>
+      },
+      {
+        path:'/',
+        element:<Home></Home>
       }
     ]
   },
 ]);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <RouterProvider router={router} />
-); 
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// or send to an analytics endpoint. Learn more
