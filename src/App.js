912ddfc4045d63fd975f26d062456102f437bb14
GAUTAM KUMAR PILLA <you@example.com>
2023-03-14 22:40:27 +0530

    new-updates

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import { Outlet } from 'react-router-dom';
 import './App.css';
 import Header from './Components/Header';
+import { Outlet } from 'react-router-dom';
 import { store } from './app/store'
 import { Provider } from 'react-redux'
-
 function App() {
   return (
     <Provider store={store}>
       <div>
-     <Header></Header>
-     <Outlet></Outlet>
-    </div>
+        <Header></Header>
+        <Outlet></Outlet>
+      </div>
     </Provider>
   );
 }
--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
-
+import { fooditemApi } from '../services/fooditems'
+import cartReducer from '../features/Cart/cartSlice'
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    cart:cartReducer,
+    [fooditemApi.reducerPath]:fooditemApi.reducer
+  },
+  middleware:(getDefaultMiddleware)=>
+    getDefaultMiddleware().concat(fooditemApi.middleware)
+  
 })
