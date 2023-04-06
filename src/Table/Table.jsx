57381dc471102a172a6a35cf2ca56366d3d784a8
GAUTAM KUMAR PILLA <you@example.com>
2023-04-06 11:13:15 +0530

    new

---
+++ src/Table/Table.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Table = () => {
+    <table>
+        <thead>                 
+            <tr>
+                <td>Firstname</td>
+                <td>Lastname</td>
+                <td>Age</td>
+                <td>Gender</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+  return (
+    <div>                  
+            
+    </div>
+  )
+}
+
+export default Table;
